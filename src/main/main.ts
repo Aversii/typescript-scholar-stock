8e1e69a8f7de6b59c9821d5e1790d573e30844db
aversii <lucasaversi@gmail.com>
2024-06-22 21:22:15 -0300

    Feat: (UseCases)  Gateway and creatUserUseCase

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,4 @@
 import { User } from "./domain/entities/user"
 
-const newUser = User.create('John Doe', 'john.doe@example.com', 'securepassword');
+const newUser = User.create('John Doe', 'john.doeexample.com', 'securepassword');
 console.log(newUser.getId())
